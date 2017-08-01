c0689bdaaff72794ad0ceb4313f6b2daca0102dd
FishInPool <huafen999@126.com>
2017-08-01 11:18:59 +0800

    Add files via upload

--- duo1/app.js
+++ duo1/app.js
@@ -1,5 +1,21 @@
 App({
   globalData:{
-    doubanBase:"https://api.douban.com"
+    doubanBase:"https://api.douban.com",
+    userInfo: null
+  },
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.getUserInfo({
+        withCredentials: false,
+        success: function (res) {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+        }
+      })
+    }
   }
 })
